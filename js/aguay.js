80446698bbe7753f03cdb4bc7badfd6c57aba11e
Enzo Vergara <enzoamb2@hotmail.com>
2017-08-30 20:56:38 -0300

    Init

---
+++ js/aguay.js
@@ -0,0 +1,11 @@
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        direction: 'vertical',
+               // scrollbar: '.swiper-scrollbar',
+        // scrollbarHide: false,
+        slidesPerView: 1,
+        grabCursor: true,
+                mousewheelControl: true
+    });
